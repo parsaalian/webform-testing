6e0f8a394341ee88a16c833508870b10d07d61b3
Parsa Alian <parsaalian0@gmail.com>
2023-05-15 14:24:27 -0700

    created HuggingFace LLM notebook for LLM testing

--- minijax/minijax/form/gpt3_handler.py
+++ minijax/minijax/form/gpt3_handler.py
@@ -7,6 +7,19 @@ from minijax.crawler.driver import get_driver_container
 from minijax.crawler.utils import get_element_xpath
 
 
+# example prompt
+'''
+You are an agent filling forms on a website. You can issue these commands:
+    - FILL////X////"Y": Fill the <input> or <textarea> element that has a relative xpath X with value Y
+    - SELECT////X////"Y": Select the option Y in the <select> element that has a relative xpath X
+Each command must be issued in a new line, with no preceding or trailing text values.
+You are given a form to fill. The form's html is as follows:
+<form _ngcontent-jwh-c36="" novalidate="" method="get" id="search-owner-form" class="form-horizontal ng-untouched ng-pristine ng-valid ng-submitted"><div _ngcontent-jwh-c36="" class="form-group"><div _ngcontent-jwh-c36="" id="lastNameGroup" class="control-group"><label _ngcontent-jwh-c36="" class="col-sm-2 control-label">Last name </label><div _ngcontent-jwh-c36="" class="col-sm-10"><input _ngcontent-jwh-c36="" size="30" maxlength="80" id="lastName" name="lastName" value="" class="form-control ng-untouched ng-pristine ng-valid"><span _ngcontent-jwh-c36="" class="help-inline"></span></div></div></div><div _ngcontent-jwh-c36="" class="form-group"><div _ngcontent-jwh-c36="" class="col-sm-offset-2 col-sm-10"><button _ngcontent-jwh-c36="" type="submit" class="btn btn-default">Find Owner</button></div></div></form>
+The xpath in this format must be relative to the form element. Just give me the commands and nothing else.
+YOUR COMMANDS:
+'''
+
+
 cfg = Config()
 driver = get_driver_container().get_driver()
 
