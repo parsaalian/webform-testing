256ce8814ae02fc561e22793c265313433f0602a
nashid <noor.nashid1@gmail.com>
2023-07-27 00:50:19 -0700

    llama2 code - 3

--- evaluation/opensource-llms/llama/llama2.py
+++ evaluation/opensource-llms/llama/llama2.py
@@ -2,22 +2,17 @@ from transformers import AutoTokenizer, pipeline, logging
 from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 import constants
 
-def load_model_llama2_70b(model_name_or_path="TheBloke/Llama-2-70B-chat-GPTQ",
-                          model_basename="gptq_model-4bit--1g", use_triton=False):
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True, device_map="auto")
 
-    model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
-                                               model_basename=model_basename,
-                                               inject_fused_attention=False,
-                                               use_safetensors=True,
-                                               trust_remote_code=False,
-                                               use_triton=use_triton,
-                                               quantize_config=None)
-    return tokenizer, model
+def load_model_llama(model_type, use_triton=False):
+    if model_type == '70B':
+        model_name_or_path = "TheBloke/Llama-2-70B-chat-GPTQ"
+        model_basename = "gptq_model-4bit--1g"
+    elif model_type == '13B':
+        model_name_or_path = "TheBloke/Llama-2-13B-chat-GPTQ"
+        model_basename = "gptq_model-4bit-128g"
+    else:
+        raise ValueError('Invalid model_type. Choose either "70B" or "13B".')
 
-
-def load_model_llama2_13b(model_name_or_path="TheBloke/Llama-2-13B-chat-GPTQ",
-                          model_basename="gptq_model-4bit-128g", use_triton=False):
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True, device_map="auto")
 
     model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
@@ -45,7 +40,7 @@ def run_inference(tokenizer, model, system_message, prompt):
 
 def run_inference_70b():
     # Load the model - load_model_llama2_70b
-    tokenizer, model = load_model_llama2_70b()
+    tokenizer, model = load_model_llama("70B")
 
     # Run inference
     system_message = constants.system_message
@@ -55,9 +50,10 @@ def run_inference_70b():
     print("\n\n*** output LLAMA (70B).....:")
     print(response)
 
+
 def run_inference_13b():
     # Load the model - load_model_llama2_13b
-    tokenizer, model = load_model_llama2_13b()
+    tokenizer, model = load_model_llama("13B")
 
     # Run inference
     system_message = constants.system_message
@@ -66,6 +62,7 @@ def run_inference_13b():
     response = run_inference(tokenizer, model, system_message, prompt)
     print("\n\n*** output LLAMA (13B).....:")
     print(response)
+
 
 if __name__ == "__main__":
     run_inference_70b()
